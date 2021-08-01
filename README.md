2c90395300c404e5197e4340c0fac426cf35427d
Hari-2008 <85682053+Hari-2008@users.noreply.github.com>
2021-08-01 12:18:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# zomie-crush-3
+# zombie-crush-boilerplate
+boilerplate for crush the zombie game
---
+++ sketch.js
@@ -0,0 +1,106 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var stones = [];
+
+function preload(){
+  zombie1 = loadImage("./assets/zombie1.png");
+  zombie2 = loadImage("./assets/zombie2.png");
+  zombie3 = loadImage("./assets/zombie3.png");
+  zombie4 = loadImage("./assets/zombie4.png");
+
+  zombiecry = loadImage("./assets/zombie.png");
+  backgroundImage = loadImage("./assets/background.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  ground = new Base(0, height - 10, width * 2, 20);
+  leftWall = new Base(100, height - 300, 200, height / 2 + 100);
+  rightWall = new Base(width - 100, height - 300, 200, height / 2 + 100);
+
+  bridge = new Bridge(22, { x: 50, y: height / 2 - 100 });
+  jointPoint = new Base(width - 250, height / 2 - 80, 40, 20);
+
+  Matter.Composite.add(bridge.body, jointPoint);
+  jointLink = new Link(bridge, jointPoint);
+
+  for (var i = 0; i <= 8; i++) {
+    var x = random(width / 2 - 200, width / 2 + 300);
+    var y = random(-100, 100);
+    var stone = new Stone(x, y, 80, 80);
+    stones.push(stone);
+  }
+
+  zombie = createSprite(width / 2, height - 110);
+  zombie.addAnimation("lefttoright", zombie1, zombie2, zombie1);
+  zombie.addAnimation("righttoleft", zombie3, zombie4, zombie3);
+  zombie.scale = 0.1;
+  zombie.velocityX = 10;
+
+  breakButton = createImg("./assets/axe.png");
+  breakButton.position(width - 200, height / 2 - 50);
+  breakButton.size(50,50)
+  //breakButton.class("breakbutton");
+  breakButton.mouseClicked(handleButtonPress);
+}
+
+function draw() {
+  background(backgroundImage);
+  Engine.update(engine);
+
+  ground.show()
+  //fill("brown")
+  //leftWall.show()
+  //rightWall.show()
+  bridge.show();
+
+  for(var stone of stones){
+    stone.show()
+  }
+
+  if (zombie.position.x >= width - 300) {
+    zombie.velocityX = -10;
+    zombie.changeAnimation("righttoleft");
+  }
+
+  if (zombie.position.x <= 300) {
+    zombie.velocityX = 10;
+    zombie.changeAnimation("lefttoright");
+  }
+
+  for(var stone of stones){
+    stone.show();
+    var pos = stone.body.position;
+    var distance = dist(zombie.position.x,zombie.position.y,pos.x,pos.y);
+    if(distance<= 20){
+      zombie.velocityX = 0;
+      Matter.Body.setVelocity(stone.body,{x:10,y:-10});
+      zombie.changeImage("zombie.png");
+      collided = true;
+
+    }
+
+
+  }
+  drawSprites();
+}
+
+function handleButtonPress(){
+  jointLink.detach();
+  //bridge.break()
+  setTimeout(()=>{
+    bridge.break();
+
+  },1500);
+}
